f13e33af131d52b039237aeb48d98a3191f78725
xuewen <jiaxuewen2006@163.com>
2017-04-18 18:10:00 +0800

    add chart tpl

--- source/js/controller/chartCtrl.js
+++ source/js/controller/chartCtrl.js
@@ -7,6 +7,30 @@ app.controller('chartCtrl',['$scope','$injector',
 			    	$scope.newList = resp.data;
 			    });
 			    document.title = "统计图表";
+
+			    $scope.projectStatistics = {
+			    	"startDate"	:"请选择起始时间",
+			    	"endDate"	:"请选择截止时间",
+			    	"panel_1":true,
+			    	"panel_2":false,
+			    	"panel_3":false,
+			    	"panel_4":false,
+			    	"switch":function(id){
+			    		$scope.projectStatistics.panel_1 = false;
+			    		$scope.projectStatistics.panel_2 = false;
+			    		$scope.projectStatistics.panel_3 = false;
+			    		$scope.projectStatistics.panel_4 = false;
+			    		$scope.projectStatistics["panel_"+id] = true;
+			    	}
+			    };
+			    $scope.contract = {
+			    	"forcus":false,
+			    	"searchLabel":function(){
+			    		$scope.contract.forcus = !$scope.contract.forcus;
+			    		console.log($scope.contract.forcus);
+			    	}
+			    };
+			    
 			}
 		);
 	}
