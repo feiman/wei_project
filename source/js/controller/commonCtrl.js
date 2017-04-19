1fd47b014ae14451a30861925e76687523470901
xuewen <jiaxuewen2006@163.com>
2017-04-19 10:02:19 +0800

    task page complete

--- source/js/controller/commonCtrl.js
+++ source/js/controller/commonCtrl.js
@@ -1,13 +1,28 @@
-app.controller('commonCtrl',['$scope','$injector','$stateProvider',
-	function($scope,$injector,$stateProvider){
+app.controller('commonCtrl',['$scope','$injector','$location',
+	function($scope,$injector,$location){
 		$injector.invoke(
 			function () {
-				$scope.activeFootBar = function(){
-					console.log($stateProvider.state.url());
-					
-				}
 				
+				$scope.activeFootBar = (function(){
+					var path  = $location.path();
+					$scope.state = {
+						"footBar_1":false,
+						"footBar_2":false,
+						"footBar_3":false,
+						"footBar_4":false
+					};
+					if(path == "/suggest"){
+						$scope.state.footBar_4 = true;
+					}else if(path == "/task"){
+						$scope.state.footBar_1 = true;
+					}else if(path == "/chart"){
+						$scope.state.footBar_2 = true;
+					}else if(path == "/message"){
+						$scope.state.footBar_3 = true;
+					}
+				})();
 			}
 		);
 	}
-]);
+])
+;
--- source/js/controller/indexCtrl.js
+++ source/js/controller/indexCtrl.js
@@ -6,6 +6,18 @@ app.controller('indexCtrl',['$scope','$injector',
 			    indexLouder.getListinfo().then(function(resp){
 			    	$scope.newList = resp.data;
 			    });
+
+			    $scope.indexInfo = {
+			    	"panel_1":true,
+			    	"panel_2":false,
+			    	"panel_3":false,
+			    	"switch":function(id){
+			    		$scope.indexInfo.panel_1 = false;
+			    		$scope.indexInfo.panel_2 = false;
+			    		$scope.indexInfo.panel_3 = false;
+			    		$scope.indexInfo["panel_"+id] = true;
+			    	}
+			    };
 			    document.title = "任务中心";
 			}
 		);
